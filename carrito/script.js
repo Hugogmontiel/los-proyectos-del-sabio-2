ed4080528411d069f07f5865a60201e53d8ecb46
COMPU-S038\daw2 <hugo16th@gmail.com>
2024-10-16 16:50:22 +0200

    16/10 carrito

--- carrito/script.js
+++ carrito/script.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", getData);
+import carrito from "./carrito.js"
+document.addEventListener("DOMContentLoaded", insertarTabla);
 
 const mas = document.querySelector(".mas"); 
 const menos = document.querySelector(".menos");
@@ -6,6 +7,7 @@ const contador = document.querySelector(".contador");
 const unidad = document.querySelector(".valorUnidad");
 const total = document.querySelector(".valorTotal");
 const totalCarrito = document.querySelector(".right"); 
+const tabla = document.querySelector('.tabla-main');
 
 let valorContador = 0;
 const valorUnidad = parseFloat(unidad.textContent.replace('€', '').replace(',', '.'));
@@ -36,11 +38,40 @@ function actualizarTotal () {
 mas.addEventListener("click", botonMas);
 menos.addEventListener("click", botonMenos);
 
-function getData(){
-    fetch('http://jsonblob.com/1295396315914428416')
-        .then(res=>res.json)
-        .then(datos=>{
-            console.log(datos);
-        })
+const carro = new carrito();
+
+
+function cargarJSON(data) {
+    carro.setlistaProductos(data.products);
+    carro.currency = data.currency;
+    console.log(carro.getListaProductos());
+
+
 }
+
+fetch('https://jsonblob.com/api/1295753236949360640')
+.then((resp) => resp.json())
+.then(function(data) {
+    console.log(data)
+    cargarJSON(data)
+    
+  })
+.catch(function(error) {
+  console.log(error);
+});
+
+
+function insertarTabla() {
+    const listuca = carro.getListaProductos();
+    listuca.array.forEach(element => {
+        
+        
+    });
+}
+        
+
+
+
+
+
 
